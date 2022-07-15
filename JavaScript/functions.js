490edd59c78721daea6616a8fd171482ce6b6ed6
Carlos Zalpa <nedhel.90@gmail.com>
2022-07-15 10:44:18 -0500

    click on buttons

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -4,19 +4,39 @@ function getText(idtext){
 function setText(text){
     document.getElementById('screenCal').value = text
 }
-
-/*document.getElementById("btn1").onclick = setText('screenCal',1);
-document.getElementById("btn2").onclick = setText('screenCal',2);
-document.getElementById("btn3").onclick = setText('screenCal',3);
-document.getElementById("btn4").onclick = setText('screenCal',4);
-document.getElementById("btn5").onclick = setText('screenCal',5);
-document.getElementById("btn6").onclick = setText('screenCal',6);
-document.getElementById("btn7").onclick = setText('screenCal',7);
-document.getElementById("btn8").onclick = setText('screenCal',8);
-document.getElementById("btn9").onclick = setText('screenCal',9);
-document.getElementById("btn0").onclick = setText('screenCal',0);*/
-window.addEventListener('load',()=>{
-    document.getElementById("btn1").addEventListener("click", setText(1))
-    document.getElementById("btn2").addEventListener("click", setText(2))
-})
+function erasechar(){
+    
+}
+ 
+window.addEventListener('load', function() {
+    console.log("entra al load")
+    document.getElementById('btn0')
+        .addEventListener('click', ()=>setText(0))
+    document.getElementById('btn1')
+        .addEventListener('click', ()=>setText(1))
+    document.getElementById('btn2')
+        .addEventListener('click', ()=>setText(2))
+    document.getElementById('btn3')
+        .addEventListener('click', ()=>setText(3))
+    document.getElementById('btn4')
+        .addEventListener('click', ()=>setText(4))
+    document.getElementById('btn5')
+        .addEventListener('click', ()=>setText(5))
+    document.getElementById('btn6')
+        .addEventListener('click', ()=>setText(6))
+    document.getElementById('btn7')
+        .addEventListener('click', ()=>setText(7))
+    document.getElementById('btn8')
+        .addEventListener('click', ()=>setText(8))
+    document.getElementById('btn9')
+        .addEventListener('click', ()=>setText(9))
+    document.getElementById('btnplus')
+        .addEventListener('click', ()=>setText('+'))
+    document.getElementById('btnminus')
+        .addEventListener('click', ()=>setText('-'))
+    document.getElementById('btntimes')
+        .addEventListener('click', ()=>setText('*'))
+    document.getElementById('btndivided')
+        .addEventListener('click', ()=>setText('/'))
+});
 
