35dca8aaad425ad8557f3149465841f66c4fb3f8
Carlos Zalpa <nedhel.90@gmail.com>
2022-07-20 16:26:39 -0500

    add equal over operations buttons

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -12,21 +12,40 @@ function eraseChar(id){
     number.pop()
     setTextOnScreen(id,number.join(''))
 }
-function setDotOnScreen(id){
+function setDotOnScreen(id){//this is to avoid repet a dot
     let number = getScreen(id)
     if(/\D/.test(number)){        
     }else{
         setCharOnScreen(id,'.')
     }
 }
-function clearAll(){
+function clearAll(){//both screens
     setTextOnScreen('screen','')
     setTextOnScreen('screenOperation','')
 }
 function operation(text){
-    setCharOnScreen('screen',text)
+    //if there is something typed
+    if(getScreen('screenOperation')!='' && getScreen('screen')!=''){
+        equal()        
+    } else if(getScreen('screenOperation')!='' && getScreen('screen')==''){        
+        let sOperation = getScreen('screenOperation').split('')
+        let sign = sOperation[sOperation.length - 1]
+        switch(sign){
+            case '+': setCharOnScreen('screen',0)
+                break;
+            case '-': setCharOnScreen('screen',0)
+                break;
+            case '/': setCharOnScreen('screen',1)
+                break;
+            case '*': setCharOnScreen('screen',1)
+                break;
+        }
+        equal() 
+    }   
+    setCharOnScreen('screen',text)     
     setTextOnScreen('screenOperation',getScreen('screen'))
     setTextOnScreen('screen','')
+    
 }
 function equal(){
     let sOperation = getScreen('screenOperation').split('')
@@ -50,7 +69,7 @@ function equal(){
     setTextOnScreen('screen',num1)
     setTextOnScreen('screenOperation','')
 }
- 
+//add events from JS
 window.addEventListener('load', function() {
     document.getElementById('btn0')
         .addEventListener('click', ()=>setCharOnScreen('screen',0))
